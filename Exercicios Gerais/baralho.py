41218a3fd9c89e82795017b990d1f63e88933eb6
Wellington Silva <wellingtonasilva45@gmail.com>
2023-05-02 16:24:41 -0300

    organização do exercício baralho da OBIm

--- Exercicios Gerais/baralho.py
+++ Exercicios Gerais/baralho.py
@@ -1,3 +1,8 @@
+# Exercicio Baralho OBI
+# https://olimpiada.ic.unicamp.br/pratique/ps/2021/f1/baralho/
+
+
+
 dados = input()
 
 P = [0 for x in range(13)]
@@ -12,23 +17,25 @@ for a in range(0,len(dados),3):
         num = conjunto[:2]
         numint = int(num)
         naipe = conjunto[2]
+
     if naipe == "P":
         if num in P:
             P[numint -1] = 14
         else:
             P[numint-1] = num
 
-
     elif naipe == "U":
         if num in U:
             U[numint -1] = 14
         else:
-            U[numint-1] = num  
+            U[numint-1] = num 
+
     elif naipe == "E":
         if num in E:
             E[numint -1] = 14
         else:
             E[numint-1] = num
+
     elif naipe == "C":
         if num in C:
             C[numint -1] = 14
@@ -47,15 +54,16 @@ faltaP = 13 - qtdp
 
 verifica_0 = 0
 zerou = 0
-
 if 14 in U:
     U = "erro"
 else:
     for verifica_0 in range(0,len(U)):
         if U[verifica_0] == 0:
             zerou += 1
+
 qtdu = 13 - zerou
 faltaU = 13 - qtdu
+
 
 verifica_0 = 0
 zeroc = 0
@@ -68,6 +76,7 @@ else:
 qtdc = 13 - zeroc
 faltaC = 13 - qtdc
 
+
 verifica_0 = 0
 zeroe = 0
 if 14 in E:
@@ -78,6 +87,7 @@ else:
             zeroe += 1
 qtde = 13 - zeroe
 faltaE = 13 - qtde
+
 
 if faltaC == 0:
     print("erro")
