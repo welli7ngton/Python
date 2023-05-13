c55fb9fdb7f0729ea47a65631cbce796dfc58cf8
Wellington Silva <wellingtonasilva45@gmail.com>
2023-05-13 17:42:24 -0300

    ultimo teste e depois vava

--- Z NOVO TESTE/maisum.py
+++ Z NOVO TESTE/maisum.py
@@ -1,12 +1,8 @@
-#Problema "maior_posicao"
-#Faça um programa que leia N números reais e armazene-os em um vetor. Em seguida, mostrar na tela
-#o maior número do vetor (supor não haver empates). Mostrar também a posição do maior elemento,
-#considerando a primeira posição como 0 (zero). 
 
 
-n = int(input("quantos numeros vai digitar? "))
+#n = int(input("quantos numeros vai digitar? "))
 
-vet : [float] = [0 for x in range(n)]
+'''vet : [float] = [0 for x in range(n)]
 
 maiornumero = 0
 pos = 0
@@ -24,5 +20,5 @@ print(f"posição do maior valor: {pos}")
 
 # listaDeNomes = ['Fulano', 'Fulana', 'DeTal', 'Ihechikara']
 
-# print(listaDeNomes.index('Fulana'))
+# print(listaDeNomes.index('Fulana'))'''
 # 1
