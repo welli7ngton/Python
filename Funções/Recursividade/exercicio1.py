91e3f0cf8fdeaab13ecfb85babffed73c2e16e35
Wellington Silva <wellingtonasilva45@gmail.com>
2023-05-15 19:31:48 -0300

    adição de atividades

--- Funções/Recursividade/exercicio1.py
+++ Funções/Recursividade/exercicio1.py
@@ -1,3 +1,13 @@
 # 1. Defina a função soma_nat que recebe como argumento um número natural  n
 #  e devolve a soma de todos os números naturais até  n
 
+def soma_nat(n):
+    if n == 0:
+        return 0
+    else:
+        return n + soma_nat(n -1)
+        n -= 1
+
+a = soma_nat(5)
+
+print(a)
---
+++ Funções/Recursividade/exercicio2.py
@@ -0,0 +1,19 @@
+# Defina a função div que recebe como argumentos dois números naturais  m
+# e  n  e devolve o resultado da divisão inteira de  m por  n
+# Neste exercício não pode recorrer às operações aritméticas de multiplicação, divisão e resto da divisão inteira
+
+
+def div(m,n):
+    if m < n:
+        return 0
+    else:
+         
+        return 1 + div(m - n, n)
+    
+
+
+
+
+a =  div(3,4)
+
+print(a)
