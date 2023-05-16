f2449582d816f9639e873c4a224d599f6b660557
Wellington Silva <wellingtonasilva45@gmail.com>
2023-05-16 19:31:53 -0300

    atividades

--- Funções/Recursividade/exercicio4.py
+++ Funções/Recursividade/exercicio4.py
@@ -1,11 +1,11 @@
 # 4. Defina a função media_digitos que recebe como argumento um número natural e devolve a média dos seus digitos.
 
 
-def media_digitos(n):
+
    
 
 
-print(media_digitos(1234))
+
  
 
 
