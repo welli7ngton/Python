c123cf416e446e034bf1d9cb34bff2c11d9945e8
Wellington Silva <wellingtonasilva45@gmail.com>
2023-05-18 16:40:53 -0300

    adição de atividades

---
+++ Funções/Recursividade/exercicio5.py
@@ -0,0 +1,7 @@
+#5. Defina a função num_perf que recebe como argumento um número inteiro positivo e devolve True se esse número for um número perfeito e False em caso contrário. Recorde que um número perfeito é um número natural que é igual à soma de todos os seus divisores próprios, isto é, a soma de todos os divisores excluindo o próprio número. Pode, se assim o entender, definir funções auxiliares.
+
+
+
+
+
+
