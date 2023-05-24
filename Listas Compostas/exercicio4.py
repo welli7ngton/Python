fc762787f9db861e823d714643e5375015b5bdf6
Wellington Silva <wellingtonasilva45@gmail.com>
2023-05-24 13:17:43 -0300

    enunciado

--- Listas Compostas/exercicio4.py
+++ Listas Compostas/exercicio4.py
@@ -1,3 +1,29 @@
+'''Implemente um programa que receba duas listas de inteiros de mesmo tamanho e troque os elementos entre as duas listas de índices pares, depois imprima os vetores modificados. 
+
+ATENÇÃO: Você somente pode imprimir as listas depois de fazer as modificações.
+
+OBS: Considere o índice 0 como sendo par.
+
+A entrada terá a seguinte ordem: A primeira informação será a quantidade de números das listas, depois os elementos da primeira lista. Depois os elementos da segunda lista.
+
+Entrada:
+4
+1 2 3 4
+7 8 9 10
+
+Saída
+7 2 9 4
+1 8 3 10
+
+Entrada:
+6
+8 4 9 5 6 1
+1 3 7 2 4 8
+
+Saída
+1 4 7 5 4 1
+8 3 9 2 6 8'''
+
 tam = int(input())
 
 l1 = [0 for x in range(tam)]
--- Listas Compostas/exercicio5.py
+++ Listas Compostas/exercicio5.py
@@ -1,3 +1,32 @@
+'''
+Implemente um programa que receba duas listas de inteiros de mesmo tamanho e depois, o valor de um índice válido dessas listas. Troque os elementos do índice informado dessas duas listas. Depois imprima listas modificadas. 
+
+ATENÇÃO: Você somente pode imprimir depois de fazer as modificações no vetor.
+
+A entrada terá a seguinte ordem: A primeira informação será a quantidade de números das listas, depois os elementos da primeira lista. Depois os elementos da segunda lista. Por fim, o índice para a realização da troca.
+
+Entrada:
+
+2       
+8 5    
+1 10
+0       
+
+Saída:
+1 5
+8 10
+
+Entrada:
+8
+2 8 8 8 8 4 4 6
+10 10 9 2 9 3 7 7
+6
+
+Saída:
+2 8 8 8 8 4 7 6
+10 10 9 2 9 3 4 7
+'''
+
 l1 = []
 l2 = []
 
