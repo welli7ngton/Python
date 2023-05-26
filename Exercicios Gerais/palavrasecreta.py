5dcf81993018d8831d03826352ce5b477588bc57
Wellington Silva <wellingtonasilva45@gmail.com>
2023-05-26 10:25:29 -0300

    correção de atividade

--- Exercicios Gerais/palavrasecreta.py
+++ Exercicios Gerais/palavrasecreta.py
@@ -1,16 +1,28 @@
 palavra = "teste"
-p = ""
-for letra in palavra:
-    p += "_"
+letra_acertada = ""
 
-print(p)
+
+
 while True:
     letra = input("digite uma letra: ")
     
-    if letra in palavra:
-        for a in range(len(palavra)):
-            if palavra[a] == letra:
-                p = p.replace(p[a], letra)
-                print(p)
-                break
+    if len(letra) > 1:
+        print("digite apenas uma letra.")
+        continue
     
+    if letra in palavra:
+        letra_acertada += letra
+    
+    palavra_formada = ""
+    for letra_s in palavra:
+        if letra_s in letra_acertada:
+            palavra_formada += letra_s
+        else:
+            palavra_formada += "_"
+
+    if palavra_formada == palavra:
+        print("voce ganhou")
+        break
+        
+        
+  
