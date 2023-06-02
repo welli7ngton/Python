9a0d93714eea201868628f72bef216e62ab43d4e
Wellington Silva <wellingtonasilva45@gmail.com>
2023-06-02 17:13:26 -0300

    contagem de tempo de execução de partes do programa

---
+++ Funções/Closure/exercicio9.py
@@ -0,0 +1,51 @@
+# Escreva uma função chamada make_timer que retorna uma função interna. A função interna deve calcular o tempo 
+# decorrido desde a primeira chamada e retornar o resultado. Teste a função retornada para medir o tempo de 
+# execução de diferentes partes do seu código.
+
+import time
+
+# inicio = time.perf_counter()
+
+# for a in range(3):
+#     time.sleep(3)
+
+# fim = time.perf_counter()
+# tempo_decorrido = fim - inicio
+# tempo_decorrido = float(tempo_decorrido)
+# print(f"Tempo decorrido:", {tempo_decorrido}, "segundos")
+
+def make_timer():
+    inicio = fim = 0
+    def contador():
+        nonlocal fim, inicio
+        fim = time.perf_counter()
+        tempo = fim - inicio
+        return  tempo
+    inicio = time.perf_counter()
+    return contador
+
+r = make_timer()
+
+for a in range(2):
+    time.sleep(2)
+
+print(r())
+
+r = make_timer()
+
+for a in range(3):
+    time.sleep(3)
+
+print(r())
+
+r = make_timer()
+
+for a in range(5):
+    time.sleep(1)
+
+print(r())
+
+# Saídas:
+# 4.000861800002895
+# 9.000454200002423
+# 5.001692300000286
