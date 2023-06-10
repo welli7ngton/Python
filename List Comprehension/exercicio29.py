5328abcfde562e971a1bb509e2ed89cae6e48818
Wellington Silva <wellingtonasilva45@gmail.com>
2023-06-09 21:01:44 -0300

    atividades ainda não resolvidas

---
+++ List Comprehension/exercicio29.py
@@ -0,0 +1,2 @@
+# Dada uma lista de strings, crie uma nova lista com as strings que têm pelo menos 
+# uma vogal repetida usando list comprehension.
---
+++ List Comprehension/exercicio30.py
@@ -0,0 +1,3 @@
+# Dada uma lista de dicionários com informações de produtos (nome, preço), crie uma 
+# nova lista apenas com os nomes dos produtos cujo preço é igual à média dos preços 
+# usando list comprehension.
