05782723529d1ac1119e87ca3044fa10b72487f1
welli7ngton - Linux <wellingtonasilva45@gmail.com>
2023-06-17 10:07:10 -0300

    atividade incompleta

---
+++ POO/Exercicios/Unidade 1/exercicio16.py
@@ -0,0 +1,3 @@
+# 6- Crie uma classe chamada "ContaPoupança" que herda da classe "ContaBancária" (do exercício fácil 4) e possui um 
+# atributo de instância "juros" e um método "calcular_juros" que calcula o juros sobre o saldo da conta.
+
