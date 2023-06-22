3d3c7fa0d81d66af9f01e32beb6a7afd18e582a2
welli7ngton - Linux <wellingtonasilva45@gmail.com>
2023-06-22 18:10:30 -0300

    teoria sobre polimosfismo

---
+++ POO/Curso OM/_polimorfismo.py
@@ -0,0 +1,9 @@
+# Teoria
+
+# polimorfismo é o princípio que permite que classes derivadas de uma mesma super classe tenham métodos iguais(com mesma assinatura) mas comportamentos diferentes.
+
+# assinatura de método = mesmo nome e quantidade de parametros(o retorno não faz parte da assinatura)
+# opinião + princípios que contam:
+# SO "L" ID
+# princípio da substituição de liskov
+# objetos de uma superclasse devem ser substituiveis por objetos de uma subclasse sem quebrar a aplicação
