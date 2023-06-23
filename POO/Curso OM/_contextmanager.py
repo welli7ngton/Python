6d7fb7f9c67fca85d3ced4b77a0c4f4b92703dd1
welli7ngton - Linux <wellingtonasilva45@gmail.com>
2023-06-23 17:50:45 -0300

    aula um sobre context manager

---
+++ POO/Curso OM/_contextmanager.py
@@ -0,0 +1,47 @@
+# Context Manager com classes - Criando e Usando gerenciadores de contexto
+# Você pode implementar seus próprios protocolos
+# apenas implementando os dunder methods que o
+# Python vai usar.
+# Isso é chamado de Duck typing. Um conceito
+# relacionado com tipagem dinâmica onde o Python não
+# está interessado no tipo, mas se alguns métodos existem
+# no seu objeto para que ele funcione de forma adequada.
+# Duck Typing:
+# Quando vejo um pássaro que caminha como um pato, nada como
+# um pato e grasna como um pato, eu chamo aquele pássaro de pato.
+# Para criar um context manager, os métodos __enter__ e __exit__
+# devem ser implementados.
+# O método __exit__ receberá a classe de exceção, a exceção e o
+# traceback. Se ele retornar True, exceção no with será
+# suprimidas.
+#
+# Ex:
+# with open('aula149.txt', 'w') as arquivo:
+#     ...
+
+class MYcontextmanager:
+
+    def __init__(self, caminho_arquivo,modo):
+        print("INIT")
+        self.caminho_arquivo = caminho_arquivo
+        self.modo = modo
+        self._arquivo = None
+      
+    def __enter__(self):
+        print("Abrindo arquivo")
+        self._arquivo =  open(self.caminho_arquivo,self.modo)
+        return self._arquivo
+
+    def __exit__(self,class_exception, exception_,traceback_):
+        print("Fechando arquivo") 
+        self._arquivo.close()
+
+instancia = MYcontextmanager("aula149.txt","w")
+
+with instancia as arquivo:
+    arquivo.write("linha 1\n")
+    arquivo.write("linha 2\n")
+    arquivo.write("linha 3\n")
+    arquivo.write("linha 4\n")
+    print("WITH")
+    
