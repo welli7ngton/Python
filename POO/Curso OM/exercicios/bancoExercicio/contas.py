234a99aca04e583dded6a860c4bcaea9ef1ee96e
welli7ngton - Linux <wellingtonasilva45@gmail.com>
2023-06-30 12:17:19 -0300

    solução exercicio banco por OM

--- POO/Curso OM/exercicios/bancoExercicio/contas.py
+++ POO/Curso OM/exercicios/bancoExercicio/contas.py
@@ -1,113 +1,85 @@
-"""
-Exercício com Abstração, Herança, Encapsulamento e Polimorfismo
-Criar um sistema bancário (extremamente simples) que tem clientes, contas e
-um banco. A ideia é que o cliente tenha uma conta (poupança ou corrente) e que
-possa sacar/depositar nessa conta. Contas corrente tem um limite extra.
-Conta (ABC)
-    ContaCorrente
-    ContaPoupanca
-Pessoa (ABC)
-    Cliente
-        Clente -> Conta
-Banco
-    Banco -> Cliente
-    Banco -> Conta
-Dicas:
-Criar classe Cliente que herda da classe Pessoa (Herança)
-    Pessoa tem nome e idade (com getters)
-    Cliente TEM conta (Agregação da classe ContaCorrente ou ContaPoupanca)
-Criar classes ContaPoupanca e ContaCorrente que herdam de Conta
-    ContaCorrente deve ter um limite extra
-    Contas têm agência, número da conta e saldo
-    Contas devem ter método para depósito
-    Conta (super classe) deve ter o método sacar abstrato (Abstração e
-    polimorfismo - as subclasses que implementam o método sacar)
-Criar classe Banco para AGREGAR classes de clientes e de contas (Agregação)
-Banco será responsável autenticar o cliente e as contas da seguinte maneira:
-    Banco tem contas e clentes (Agregação)
-    * Checar se a agência é daquele banco
-    * Checar se o cliente é daquele banco
-    * Checar se a conta é daquele banco
-Só será possível sacar se passar na autenticação do banco (descrita acima)
-Banco autentica por um método.
-"""
+import abc
 
 
-from abc import ABC, abstractmethod
-
-
-class Conta(ABC):
-    def __init__(self, agencia, numero, saldo=0):
+class Conta(abc.ABC):
+    def __init__(self, agencia: int, conta: int, saldo: float = 0) -> None:
         self.agencia = agencia
-        self.numero = numero
+        self.conta = conta
         self.saldo = saldo
 
-    @abstractmethod
-    def sacar(self, valor_saque): ...
+    @abc.abstractmethod
+    def sacar(self, valor: float) -> float: ...
 
-    def detalhes(self, msg=""):
-        print(msg, f"Seu saldo atual é: {self.saldo:.2f}")
+    def depositar(self, valor: float) -> float:
+        self.saldo += valor
+        self.detalhes(f'(DEPÓSITO {valor})')
+        return self.saldo
 
-    def depositar(self, valor_deposito):
-        self.saldo += valor_deposito
-        self.detalhes(msg="Operação realizada.\n")
-        return True
+    def detalhes(self, msg: str = '') -> None:
+        print(f'O seu saldo é {self.saldo:.2f} {msg}')
+        print('--')
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        attrs = f'({self.agencia!r}, {self.conta!r}, {self.saldo!r})'
+        return f'{class_name}{attrs}'
 
 
 class ContaPoupanca(Conta):
-    def __init__(self, agencia, numero, saldo=0):
-        self.agencia = agencia
-        self.numero = numero
-        self.saldo = saldo
+    def sacar(self, valor):
+        valor_pos_saque = self.saldo - valor
 
-    def __repr__(self) -> str:
-        return f"Agencia({self.agencia}) | Numero({self.numero})"
+        if valor_pos_saque >= 0:
+            self.saldo -= valor
+            self.detalhes(f'(SAQUE {valor})')
+            return self.saldo
 
-    def sacar(self, valor_saque):
-        if valor_saque > self.saldo:
-            self.detalhes(msg="Saldo insuficiente, operação não realizada.\n")
-            return False
-        self.saldo -= valor_saque
-        self.detalhes(msg="Operação realizada.\n")
-        return True
+        print('Não foi possível sacar o valor desejado')
+        self.detalhes(f'(SAQUE NEGADO {valor})')
+        return self.saldo
 
 
 class ContaCorrente(Conta):
-    def __init__(self, agencia: int, numero: int,
-                 saldo: float = 0, limite: float = 0):
-        super().__init__(agencia, numero, saldo)
+    def __init__(
+        self, agencia: int, conta: int,
+        saldo: float = 0, limite: float = 0
+    ):
+        super().__init__(agencia, conta, saldo)
         self.limite = limite
 
-    def __repr__(self) -> str:
-        return f"Agencia({self.agencia}) | Numero({self.numero})"
-
-    def sacar(self, valor_saque):
-        valor_pos_saque = self.saldo - valor_saque
+    def sacar(self, valor: float) -> float:
+        valor_pos_saque = self.saldo - valor
         limite_maximo = -self.limite
 
         if valor_pos_saque >= limite_maximo:
-            self.saldo -= valor_saque
-            self.detalhes(msg="Operação realizada.\n")
-            return True
+            self.saldo -= valor
+            self.detalhes(f'(SAQUE {valor})')
+            return self.saldo
 
-        self.detalhes(msg="Operação não realizada.\n")
-        return False
-# testes
+        print('Não foi possível sacar o valor desejado')
+        print(f'Seu limite é {-self.limite:.2f}')
+        self.detalhes(f'(SAQUE NEGADO {valor})')
+        return self.saldo
 
-# cp1 = ContaPoupanca(111,222,100)
-# cp1.sacar(200)
-# cp1.sacar(99)
-# cp1.depositar(1000)
-# cp1.sacar(1001)
-# cp1.sacar(1)
+    def __repr__(self):
+        class_name = type(self).__name__
+        attrs = f'({self.agencia!r}, {self.conta!r}, {self.saldo!r}, '\
+            f'{self.limite!r})'
+        return f'{class_name}{attrs}'
 
-# cc1 = ContaCorrente(111,222,100,100)
-# cc1.sacar(1)
-# cc1.depositar(1)
-# cc1.sacar(100)
-# cc1.sacar(100)
-# cc1.sacar(100)
-# cc1.depositar(50)
-# cc1.sacar(100)
-# cc1.depositar(50)
-# cc1.depositar(100)
+
+if __name__ == '__main__':
+    cp1 = ContaPoupanca(111, 222)
+    cp1.sacar(1)
+    cp1.depositar(1)
+    cp1.sacar(1)
+    cp1.sacar(1)
+    print('##')
+    cc1 = ContaCorrente(111, 222, 0, 100)
+    cc1.sacar(1)
+    cc1.depositar(1)
+    cc1.sacar(1)
+    cc1.sacar(1)
+    cc1.sacar(98)
+    cc1.sacar(1)
+    print('##')
--- POO/Curso OM/exercicios/bancoExercicio/pessoas.py
+++ POO/Curso OM/exercicios/bancoExercicio/pessoas.py
@@ -7,39 +7,39 @@ class Pessoa:
         self.idade = idade
 
     @property
-    def get_nome(self):
-        return self.nome
+    def nome(self):
+        return self._nome
 
-    @get_nome.setter
-    def get_nome(self, nome: str) -> str:
-        self.nome = nome
+    @nome.setter
+    def nome(self, nome: str):
+        self._nome = nome
 
     @property
-    def get_idade(self):
-        return self.idade
+    def idade(self):
+        return self._idade
 
-    @get_idade.setter
-    def get_idade(self, idade: int) -> int:
-        self.idade = idade
+    @idade.setter
+    def idade(self, idade: int):
+        self._idade = idade
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        attrs = f'({self.nome!r}, {self.idade!r})'
+        return f'{class_name}{attrs}'
 
 
 class Cliente(Pessoa):
-    def __init__(self, nome: str, idade: int):
+    def __init__(self, nome: str, idade: int) -> None:
         super().__init__(nome, idade)
         self.conta: contas.Conta | None = None
 
-    def __repr__(self):
-        return f"{self.nome.title()}"
 
-
-if __name__ == "__main__":
-    c1 = Cliente("wellington", 20)
-    c2 = Cliente("maria", 19)
-    c1.conta = contas.ContaCorrente(111, 222)
-    c2.conta = contas.ContaPoupanca(333, 444)
-    print(c1.conta)
-    print(c2.conta)
-    c1.nome = "Wellington Almeida"
-    c2.nome = "Maria Helena"
+if __name__ == '__main__':
+    c1 = Cliente('Luiz', 30)
+    c1.conta = contas.ContaCorrente(111, 222, 0, 0)
     print(c1)
+    print(c1.conta)
+    c2 = Cliente('Maria', 18)
+    c2.conta = contas.ContaPoupanca(112, 223, 100)
     print(c2)
+    print(c2.conta)
