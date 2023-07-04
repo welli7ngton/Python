116f44a15e30fe79e38e804cdc3d47a5f983b275
welli7ngton - Linux <wellingtonasilva45@gmail.com>
2023-07-04 17:42:26 -0300

    organizando pasta

--- Modulos Python/__datetime/criando_datas.py
+++ Modulos Python/__datetime/criando_datas.py
@@ -6,15 +6,16 @@
 # https://pt.wikipedia.org/wiki/Era_Unix
 # datetime.fromtimestamp(Unix Timestamp)
 # https://docs.python.org/3/library/datetime.html
-
-
+# Para timezones
+# https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+# Instalando o pytz
+# pip install pytz types-pytz
 from datetime import datetime
 
-data_str = '03/07/2023'
-data_str_forma = '%d/%m/%Y'
+data_str_data = '2022/04/20 07:49:23'
+data_str_data = '20/04/2022'
+data_str_fmt = '%d/%m/%Y'
 
-
-data = datetime.strptime(data_str, data_str_forma)
+# data = datetime(2022, 4, 20, 7, 49, 23)
+data = datetime.strptime(data_str_data, data_str_fmt)
 print(data)
-
-print(datetime.now())
--- Modulos Python/exercicios/_datetime1.py
+++
@@ -1,6 +0,0 @@
-# Exercício 1: Calculadora de dias úteis
-# Crie uma classe chamada CalculadoraDiasUteis que receba uma data de início e
-# uma quantidade de dias como argumentos. A classe deve fornecer um método para
-# calcular a data final considerando apenas dias úteis
-# (excluindo sábados e domingos).
-
