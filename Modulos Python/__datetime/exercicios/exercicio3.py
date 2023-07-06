e7dc1fd07298f97f5454ddfb92e70a61cfa67cfb
welli7ngton - Linux <wellingtonasilva45@gmail.com>
2023-07-05 21:19:28 -0300

    inicio da terceira questão

---
+++ Modulos Python/__datetime/exercicios/exercicio3.py
@@ -0,0 +1,15 @@
+# Exercício 3: Rastreador de datas importantes
+# Crie uma classe chamada RastreadorDatas que receba uma lista de datas
+# como argumento. A classe deve fornecer métodos para verificar se uma
+# determinada data está presente na lista e para calcular a quantidade
+# de dias entre a data atual e a próxima data da lista.
+
+datas_importantes = ["16/06/2023",
+                     "24/01/2023",
+                     "13/06/2023",
+                     "12/12/2023",
+                     "07/12/2023"]
+
+
+
+
