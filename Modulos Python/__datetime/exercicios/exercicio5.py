548f57b8592c8b4a08739f649e73219755da2c7b
welli7ngton - Linux <wellingtonasilva45@gmail.com>
2023-07-07 20:30:04 -0300

    os.path

---
+++ Modulos Python/__datetime/exercicios/exercicio5.py
@@ -0,0 +1,10 @@
+# Exercício 5: Verificador de horário de expediente
+# Crie uma função chamada verifica_horario_expediente que receba uma data e
+# hora como argumento e verifique se está dentro do horário de expediente de
+# uma empresa (por exemplo, das 9h às 18h, de segunda a sexta-feira).
+# A função deve retornar True se estiver no horário de
+# expediente e False caso contrário.
+
+
+def verifica_horario_expediente(data, hora):
+    pass
