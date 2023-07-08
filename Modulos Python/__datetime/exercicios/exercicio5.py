4ef2591bf95ed347fb78e13d282d8f7fd6bcc1b4
welli7ngton - Linux <wellingtonasilva45@gmail.com>
2023-07-08 14:45:16 -0300

    solução do exercicio 5

--- Modulos Python/__datetime/exercicios/exercicio5.py
+++ Modulos Python/__datetime/exercicios/exercicio5.py
@@ -5,6 +5,25 @@
 # A função deve retornar True se estiver no horário de
 # expediente e False caso contrário.
 
+import calendar
+from datetime import datetime
+
 
 def verifica_horario_expediente(data, hora):
-    pass
+    data_ = datetime.strptime(data + " | " + hora,  "%d/%m/%Y | %H:%M")
+
+    nome_dia = calendar.day_name[calendar.weekday(data_.year,
+                                                  data_.month,
+                                                  data_.day)]
+    print(nome_dia)
+    if nome_dia in ["Sunday", "Saturday"]:
+        print("Dia não é útil.")
+        return False
+    print("O dia é útil.")
+    return True
+
+
+if __name__ == "__main__":
+
+    # print(calendar.monthcalendar(2023, 12))
+    verifica_horario_expediente("26/12/2023", "07:57")
