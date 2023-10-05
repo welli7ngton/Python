68b06ce7d27d20a8bace1f9c50cafc126bc9d2f3
welli7ngton <wellingtonasilva45@gmail.com>
2023-10-05 11:20:36 -0300

    criando testes para deletar e pegar um item da lista

--- TDD/test_linked_list.py
+++ TDD/test_linked_list.py
@@ -11,6 +11,7 @@ from linked_list import LinkedList
 class TestLinkedList(unittest.TestCase):
     def setUp(self) -> None:
         self.lkd = LinkedList()
+        self.lkd.add('wellington')
     
     def test_adding_none_value_to_linkedlist_getting_an_assertion_error(self):
         with self.assertRaises(Exception) as ex:
@@ -18,11 +19,19 @@ class TestLinkedList(unittest.TestCase):
         self.assertEqual(ex.exception.args[0], 'class LinkedList cant accept values diferent of: int, float, str, bool.')
 
     def test_getting_the_length_of_the_linkedlist(self):
-        self.assertEqual(self.lkd.length(), 0)
+        self.assertEqual(self.lkd.length(), 1)
+    
+    def test_getting_an_value_from_the_list(self):
+        self.assertEqual(self.lkd.get_this(0), 'wellington')
+    
+    def test_deleting_an_list_item_getting_a_true_value(self):
+        self.assertTrue(self.lkd.delete_this(0))
+
+    def test_deleting_an_list_item_getting_a_IndexError(self):
+        with self.assertRaises(IndexError) as ex:
+            self.lkd.delete_this(7)
+        self.assertEqual(ex.exception.args[0], 'list index out of range')
+
         
-            
-
-
-
 if __name__ == '__main__':
     unittest.main(verbosity=2)
