c7b6125a31ae521db27681444ba2097b71df922a
welli7ngton <wellingtonasilva45@gmail.com>
2023-10-08 18:17:11 -0300

    codigo inicial depois do primeiro teste

---
+++ TDD/to_do_list.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+import datetime
+
+
+class ToDoList:
+
+    @staticmethod
+    def add_item(title: str, date: str, time: str, description: str | None = None):
+        return True
+
+
+if __name__ == '__main__':
+    ...
