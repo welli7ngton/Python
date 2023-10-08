5e5e0cfd4fff0ad91f8f5bd4edde38e29741e5c7
welli7ngton <wellingtonasilva45@gmail.com>
2023-10-07 21:03:47 -0300

    criação de mais lógica para verificar existencia de usuários e senhas

--- TDD/simple_login_logic.py
+++ TDD/simple_login_logic.py
@@ -1,24 +1,12 @@
 # flake8: noqa
+import string
+
 
 class LoginInterface:
     _registered_users = []
-    def create_user(username: str, passwd: str, passwdconfirm: str):
-        assert not isinstance(username, None | int | float), 'User Data Error: Username cant be a None value.'
-        assert passwd == passwdconfirm, 'Password error: Passwords dont confirm.'
-        assert len(passwd) >= 8, 'Password error: too short.'
-        assert __class__.check_if_password_is_common(passwd) is True, 'Password error: Password too common.'
-        assert __class__.check_if_passwd_has_any_special_characters(passwd) is True, 'Password error: Password too weak.'
-        assert __class__.check_if_user_name_already_exists(username), 'User error: Username already exists.'
-        user = {
-            'username': username,
-            'password': passwd,
-        }
-        __class__._registered_users.append(user)
-
-        return user
 
     @classmethod
-    def check_if_password_is_common(cls, passwd: str):
+    def _check_if_password_is_common(cls, passwd: str):
         most_common_passwd = [
             '123456789', '12345678', 'password', 'qwerty12', 'chess123',
             '87654321', 'abcdefgh', 'zxcvbnm,', '11111111', '22222222',
@@ -28,7 +16,7 @@ class LoginInterface:
         return True if passwd not in most_common_passwd else False
 
     @classmethod
-    def check_if_passwd_has_any_special_characters(cls, passwd: str):
+    def _check_if_passwd_has_any_special_characters(cls, passwd: str):
         return True if (
             '!' in passwd
             or '@' in passwd
@@ -40,5 +28,66 @@ class LoginInterface:
         ) else False
 
     @classmethod
-    def check_if_user_name_already_exists(cls, username: str):
+    def _check_if_user_name_already_exists(cls, username: str):
         return True if username not in [user['username'] for user in cls._registered_users] else False
+
+    @classmethod
+    def _check_if_passwd_has_numerical_characters(cls, passwd: str):
+        for i in passwd:
+            for j in string.digits:
+                if j == i:
+                    return True
+        return False
+
+    @classmethod
+    def _check_uppercase_characters_existence(cls, passwd: str):
+        for i in passwd:
+            for j in string.ascii_uppercase:
+                if j == i:
+                    return True
+        return False
+
+    @classmethod
+    def _check_if_username_exists_in__registered_users_of_this_class(cls, username: str):
+        for user in cls._registered_users:
+            if user['username'] == username:
+                return True
+        return False
+
+    @classmethod
+    def _check_if_user_match_with_the_password(cls, username: str, old_password: str):
+        usr = [u for u in cls._registered_users if u['username'] == username]
+        
+        return True if usr[0]['password'] == old_password else False
+
+
+    def create_user(username: str, passwd: str, passwdconfirm: str):
+        assert not isinstance(username, None | int | float), 'User Data Error: username cant be a None value.'
+        assert passwd == passwdconfirm, 'Password error: passwords dont confirm.'
+        assert len(passwd) >= 8, 'Password error: too short.'
+        assert __class__._check_if_password_is_common(passwd) is True, 'Password error: password too common.'
+        assert __class__._check_if_passwd_has_any_special_characters(passwd) is True, 'Password error: password too weak.'
+        assert __class__._check_if_user_name_already_exists(username), 'User error: username already exists.'
+        assert __class__._check_if_passwd_has_numerical_characters(passwd), 'Password error: missing any numerical character.'
+        assert __class__._check_uppercase_characters_existence(passwd), 'Password error: missing any uppercase character.'
+
+        user = {
+            'username': username,
+            'password': passwd,
+        }
+
+        __class__._registered_users.append(user)
+
+        return user
+    
+    def change_password(user: str, passwd: str, new_passwd: str):
+        assert __class__._check_if_username_exists_in__registered_users_of_this_class(user), 'User error: not found.'
+        assert __class__._check_if_user_match_with_the_password(user, passwd), 'Password error: user and old password dont match.'
+        _user = dict([u for u in __class__._registered_users if u['username'] == user])
+
+        _user['password'] = new_passwd
+        return True
+
+
+if __name__ == '__main__':
+    ...
