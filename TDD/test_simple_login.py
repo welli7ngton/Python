149b2081bf5d6bbd2d7f2f94866b47d2fd1f4be2
welli7ngton <wellingtonasilva45@gmail.com>
2023-10-07 21:04:02 -0300

    testes

--- TDD/test_simple_login.py
+++ TDD/test_simple_login.py
@@ -7,14 +7,14 @@ class TestLoginInteface(unittest.TestCase):
     def test_create_user_raises_user_name_cant_be_a_none_value(self):
         with self.assertRaises(Exception) as ex:
             LoginInterface.create_user(None, 'pass', 'pass')
-        self.assertEqual(ex.exception.args[0], 'User Data Error: Username cant be a None value.')
+        self.assertEqual(ex.exception.args[0], 'User Data Error: username cant be a None value.')
 
-    def test_if_passwd_and_passwd_confirm_are_equals_raises_an_password_error(self):
+    def test_if_passwd_and_passwdconfirm_are_equals_raises_an_password_error(self):
         with self.assertRaises(Exception) as ex:
-            LoginInterface.create_user('Well', 'pass1', 'pass2')
-        self.assertEqual(ex.exception.args[0], 'Password error: Passwords dont confirm.')
+            LoginInterface.create_user('Foo', 'pass1', 'pass2')
+        self.assertEqual(ex.exception.args[0], 'Password error: passwords dont confirm.')
 
-    def test_password_length_raises_an_length_error(self):
+    def test_password_length_raises_an_password_error(self):
         with self.assertRaises(Exception) as ex:
             LoginInterface.create_user('Foo', 'p1', 'p1')
         self.assertEqual(ex.exception.args[0], 'Password error: too short.')
@@ -22,17 +22,42 @@ class TestLoginInteface(unittest.TestCase):
     def test_if_password_is_strong_enough_raises_an_password_error(self):
         with self.assertRaises(Exception) as ex:
             LoginInterface.create_user('Foo', '12345678', '12345678')
-        self.assertEqual(ex.exception.args[0], 'Password error: Password too common.')
+        self.assertEqual(ex.exception.args[0], 'Password error: password too common.')
     
     def test_username_existence_raises_an_username_already_exists(self):
         with self.assertRaises(Exception) as ex:
             LoginInterface.create_user('Foo', 'strRong%p4$$wD', 'strRong%p4$$wD')
             LoginInterface.create_user('Foo', 'strRong%p4$$wD', 'strRong%p4$$wD')
-        self.assertEqual(ex.exception.args[0], 'User error: Username already exists.')
+        self.assertEqual(ex.exception.args[0], 'User error: username already exists.')
+    
+    def test_if_passwd_has_any_digit_raises_an_password_error(self):
+        with self.assertRaises(Exception) as ex:
+            LoginInterface.create_user('Foo', 'testPassword*#', 'testPassword*#')
+        self.assertEqual(ex.exception.args[0], 'Password error: missing any numerical character.')
+    
+    def test_if_passwd_has_any_uppercase_character_raises_an_password_error(self):
+        with self.assertRaises(Exception) as ex:
+            LoginInterface.create_user('Foo', '1testpassword*#', '1testpassword*#')
+        self.assertEqual(ex.exception.args[0], 'Password error: missing any uppercase character.')
 
-    # TODO: digits in password
-    # TODO: upercase characters in password
-    # TODO: change password test
+    def test_if_user_exists_raises_an_user_error_not_found(self):
+        with self.assertRaises(Exception) as ex:
+            LoginInterface.change_password('iDontExist', 'Str0ng#P4s$w0rd!', 'n3wStr0ng#P4s$w0rd!')
+        self.assertEqual(ex.exception.args[0], 'User error: not found.')
+    
+    def test_changing_the_password_getting_a_true_value(self):
+        LoginInterface.create_user('FooW', 'StrongPassword#3', 'StrongPassword#3')
+        self.assertEqual(LoginInterface.change_password('FooW', 'StrongPassword#3', 'n###33WStrongPassword#3'), True)
+    
+    def test_if_password_match_with_the_user_getting_an_password_error(self):
+        with self.assertRaises(Exception) as ex:
+            LoginInterface.create_user('Foo', 'StrongPassword#3', 'StrongPassword#3')
+            LoginInterface.change_password('Foo', 'otherStr0n6P4$$', 'nE3WStrongPassword#3')
+        self.assertEqual(ex.exception.args[0], 'Password error: user and old password dont match.')
+
+
+        
+    # TODO: change password tests
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
