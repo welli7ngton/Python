632123371172ebd1322d6a07fdc24aa9d8d030a3
welli7ngton <wellingtonasilva45@gmail.com>
2023-10-09 13:58:18 -0300

    testes de tipagem de dados

--- TDD/test_to_do_list.py
+++ TDD/test_to_do_list.py
@@ -7,9 +7,31 @@ from to_do_list import ToDoList
 
 
 class TestToDoList(unittest.TestCase):
-    def test_return_of_add_item_function(self):
-        self.assertTrue(ToDoList.add_item('test', 'a', 'b', 'd'))
+    # type checking tests
+    def test_if_title_is_an_str_instance_getting_an_assertion_error(self):
+        with self.assertRaises(Exception) as ex:
+            ToDoList.add_item(0, '11/11/1111', '00:00', None)
+        self.assertEqual(ex.exception.args[0], 'Title is not an str instance.')
 
+    def test_if_date_is_an_str_instance_getting_an_assertion_error(self):
+        with self.assertRaises(Exception) as ex:
+            ToDoList.add_item('title', 1, '00:00', None)
+        self.assertEqual(ex.exception.args[0], 'Date is not an str instance.')
+
+    def test_if_time_is_an_str_instance_getting_an_assertion_error(self):
+        with self.assertRaises(Exception) as ex:
+            ToDoList.add_item('title', '11/11/1111', 0, None)
+        self.assertEqual(ex.exception.args[0], 'Time is not an str instance.')
+
+
+    def test_if_description_is_an_str_instance_or_none_value_getting_an_assertion_error(self):
+        with self.assertRaises(Exception) as ex:
+            ToDoList.add_item('title', '11/11/1111', '00:00', 0)
+        self.assertEqual(ex.exception.args[0], 'Description is not an str instance or none value.')
+
+    def test_if_date_can_be_converted_to_datetime_with_strptime_function_raises_an_error(self):
+        with self.assertRaises(Exception) as ex:
+            self.assertEqual(ToDoList.add_item('test_item', '11|11|1111', '00:00', None), 'time data does not march with the default format.')
 
 
 if __name__ == '__main__':
