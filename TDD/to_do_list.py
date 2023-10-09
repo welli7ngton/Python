f7d09b589ba455616c2ebb4890b2960e8fb09dff
welli7ngton <wellingtonasilva45@gmail.com>
2023-10-09 14:00:01 -0300

    código funcional após os testes de tipagem de dados

--- TDD/to_do_list.py
+++ TDD/to_do_list.py
@@ -1,11 +1,20 @@
 # flake8: noqa
-import datetime
+from datetime import datetime
 
 
 class ToDoList:
+    DEFAULT_DATE_FORMAT = '%d/%m/%Y'
 
     @staticmethod
     def add_item(title: str, date: str, time: str, description: str | None = None):
+        assert isinstance(title, str), 'Title is not an str instance.'
+        assert isinstance(date, str), 'Date is not an str instance.'
+        assert isinstance(time, str), 'Time is not an str instance.'
+        assert isinstance(description, str) or description is None, 'Description is not an str instance or none value.'
+        try:
+            converted_date = datetime.strptime(date, __class__.DEFAULT_DATE_FORMAT)
+        except AssertionError:
+            raise Exception('time data does not march with the default format.')
         return True
 
 
