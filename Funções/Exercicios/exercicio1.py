d40ae11bdcc95ca6eff867c580ff563c432dab7d
welli7ngton <wellingtonasilva45@gmail.com>
2023-10-11 14:13:07 -0300

    refatorando código

--- Funções/Exercicios/exercicio1.py
+++ Funções/Exercicios/exercicio1.py
@@ -1,28 +1,27 @@
-#Faça	uma	função	que calcule a	média	de	um	aluno	de	acordo	com	o	critério	definido	
-#neste	curso.	Além	disso,	 faça	uma	segunda	 função	que	informe	o	status	do	aluno	de	
-#acordo	com	a	tabela	a	seguir:
+# Faça	uma	função	que calcule a	média	de	um	aluno	de	acordo	com	o
+# critério	definido. Além	disso,	 faça	uma	segunda	 função	que	informe	o
+# status	do	aluno	de acordo	com	a	tabela	a	seguir:
 #   Nota	acima	de	6	à “Aprovado”
 #   Nota	entre	4	e	6	à Conceito	“Verificação	Suplementar”
 #   Nota	abaixo	de	4	à Conceito	“Reprovado”
 
 
+class CalculateGrades:
+    def __init__(self, note1: int | float, note2: int | float) -> None:
+        self.get_result(note1, note2)
+
+    def get_result(self, n1, n2) -> str:
+        if (n1 + n2) / 2 >= 6:
+            print('Approved.')
+            return
+
+        if (n1 + n2) / 2 < 6 and (n1 + n2) / 2 >= 4:
+            print('Supplemental Verification.')
+            return
+
+        print('Disapproved.')
+        return
 
 
-def calculamedia(nota1,nota2):
-    media = 0
-    media = (nota1 + nota2) / 2
-
-    return(media)
-
-print("Digite suas notas n1 e n2:")
-n1 = float(input())
-n2 = float(input())
-
-resultado = calculamedia(n1, n2)
-
-if resultado >= 6:
-    print("Aprovado.")
-elif resultado < 6 and resultado >= 4:
-    print("Verificação Suplementar.")
-else:
-    print("Reprovado.")
+if __name__ == '__main__':
+    CalculateGrades(10, 10)
