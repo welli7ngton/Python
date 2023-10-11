6ead1f44f6c1d388965ca61320cf6ac5b3bacc14
welli7ngton <wellingtonasilva45@gmail.com>
2023-10-11 14:00:50 -0300

    refatorando código

--- Funções/Exercicios/exercicio16.py
+++ Funções/Exercicios/exercicio16.py
@@ -1,34 +1,16 @@
-#Faça um programa que recebe três números do usuário, e identifica o maior através de uma função e o menor número
-#através de outra função.Achar maior e menor número com funções
+# Faça um programa que recebe três números do usuário, e identifica o maior
+# através de uma função e o menor número através de outra função.Achar maior
+# e menor número com funções
 
 
-def maior(n1, n2, n3):
-    if n1 > n2 and n1 > n3:
-        m = n1
-    elif n2 > n3:
-        m = n2
-    else:
-        m = n3
+def higher(n1: int | float, n2: int | float, n3: int | float) -> int | float:
+    return max(n1, n2, n3)
 
-    return(m)
 
-def menor(a, b, c):
-    if a < b and a < c:
-        me = a
-    elif b < c:
-        me = b
-    else:
-        me = c
-    return(me)
+def lower(n1: int | float, n2: int | float, n3: int | float) -> int | float:
+    return min(n1, n2, n3)
 
-print("digite tres numeros: ")
 
-num1 = int(input())
-num2 = int(input())
-num3 = int(input())
-
-maiorn = maior(num1, num2,num3)
-menorn = menor(num1, num2, num3)
-
-print(f"o maior é: {maiorn}")
-print(f"o menor é: {menorn}")
+if __name__ == '__main__':
+    print(higher(1, 2, 3))
+    print(lower(1, 2, 3))
