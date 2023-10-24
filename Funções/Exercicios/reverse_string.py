496a1f2385cedc8040b8fdaaeb767f7779e346ff
welli7ngton <wellingtonasilva45@gmail.com>
2023-10-24 13:57:16 -0300

    adicionando novo jeito de reverter uma string

--- Funções/Exercicios/reverse_string.py
+++ Funções/Exercicios/reverse_string.py
@@ -1,6 +1,14 @@
+class ReverseString:
+    @classmethod
+    def reverse(cls, s: str):
+        return ''.join([s[i - 1] for i in range(len(s), 0, -1)])
+
+
 def reverse_str(_string: str) -> str:
     return ''.join([_string[i - 1] for i in range(len(_string), 0, -1)])
 
 
 if __name__ == '__main__':
     print(reverse_str('wellington'))
+
+    print(ReverseString.reverse('): oot skrow ti'))
