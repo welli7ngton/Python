beda714dff66a16f49255fe75c1d8c4b403318a6
welli7ngton <wellingtonasilva45@gmail.com>
2024-05-23 23:43:07 -0300

    solution

--- Exercicios Gerais/binary-search.py
+++ Exercicios Gerais/binary-search.py
@@ -9,9 +9,9 @@ def binary_search(arr, target):
         if arr[mid] > target:
             high = mid - 1
         else:
-            low == mid + 1
+            low = mid + 1
     return None
 
 
 if __name__ == '__main__':
-    print(binary_search([1, 3, 5, 7, 9], 3))
+    print(binary_search([1, 3, 5, 7, 9], 9))
