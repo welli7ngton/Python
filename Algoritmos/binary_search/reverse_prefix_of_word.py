ea2b968565d40147007215fc9c8aa5e2f94bff3d
welli7ngton <wellingtonasilva45@gmail.com>
2024-05-25 12:07:21 -0300

    solucao reverse_prefix_of_word

---
+++ Algoritmos/binary_search/reverse_prefix_of_word.py
@@ -0,0 +1,30 @@
+# Leetcode 2000: Reverse Prefix of Word
+
+def reversePrefix(word, ch):
+    prefix = ''
+    if ch not in word:
+        return word
+    for char in word:
+        if char != ch:
+            prefix += char
+
+        else:
+            prefix += char
+            break
+
+    left = 0
+    right = len(prefix) - 1
+
+    reversed_prefix = ['' for x in range(right + 1)]
+    while left <= right:
+        reversed_prefix[left], reversed_prefix[right] = prefix[right], prefix[left]
+        left += 1
+        right -= 1
+
+    reversed_prefix = "".join([x for x in reversed_prefix])
+
+    return reversed_prefix + word[len(prefix):]
+
+
+if __name__ == '__main__':
+    print(reversePrefix('abcdefd', 'd'))
