f8b98714ede0492da8fc8422030f65c098416138
Wellington - Trabalho <wellingtonasilva45@gmail.com>
2024-12-06 13:44:10 -0300

    refactor: adiciona caractere de quebra de linha evitando o uso de vários 'print'

--- Exercicios Gerais/baralho2.py
+++ Exercicios Gerais/baralho2.py
@@ -38,8 +38,5 @@ for i in range(0, len(dados), 3):
 
 cartasC, cartasE, cartasU, cartasP = divide_naipes(lista_dados)
 
-print(retorna_falta(cartasC))
-print(retorna_falta(cartasE))
-print(retorna_falta(cartasU))
-print(retorna_falta(cartasP))
+print(f"{retorna_falta(cartasC)}\n{retorna_falta(cartasE)}\n{retorna_falta(cartasU)}\n{retorna_falta(cartasP)}")
 
